3e3325470ce6eeecda9ada19f1aa6b363ed8411f
jiosue <joe.iosue@yahoo.com>
2019-01-25 13:02:18 -0800

    Add files via upload

--- qSonify/maps/__init__.py
+++ qSonify/maps/__init__.py
@@ -3,10 +3,10 @@ Mapping function take in arguments to define the mapping. They return another
 function of the form mapping(res, name, tempo) that returns a Song object.
 """
 
-from qSonify.maps.fermionic import *
-from qSonify.maps.scale import *
-from qSonify.maps.grandpiano import *
-from qSonify.maps.stringquartet import *
-from qSonify.maps.frequencymapping import *
+from qSonify.maps.fermionic import fermionic
+from qSonify.maps.scale import scale
+from qSonify.maps.grandpiano import grandpiano
+from qSonify.maps.stringquartet import stringquartet
+from qSonify.maps.frequencymapping import frequencymapping
 
 default_map = fermionic()

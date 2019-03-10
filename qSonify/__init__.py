020b839c656f289be6b68d3e0f6bfda56ee74cc5
jiosue <joe.iosue@yahoo.com>
2019-03-09 17:42:09 -0800

    Add files via upload

--- qSonify/__init__.py
+++ qSonify/__init__.py
@@ -2,3 +2,6 @@ from qSonify.qc import *
 from qSonify import maps
 from qSonify.sonify import *
 from qSonify.qSonify import alg_to_song
+
+state_to_decimal = lambda s: int(s, base=2)
+decimal_to_state = lambda x, num_qubits: ("{0:0%db}" % num_qubits).format(x)

f9b1b5f09faf193d7dc74897298b2ab82c7a3cc8
Joseph T. Iosue <joe.iosue@yahoo.com>
2019-07-18 14:03:18 -0700

    Update __init__.py

--- qSonify/__init__.py
+++ qSonify/__init__.py
@@ -1,7 +1,7 @@
 from qSonify.qc import *
 from qSonify import maps
 from qSonify.sonify import *
-from qSonify.qSonify import alg_to_song
+from qSonify._qSonify import alg_to_song
 from ._version import __version__
 
 state_to_decimal = lambda s: int(s, base=2)
